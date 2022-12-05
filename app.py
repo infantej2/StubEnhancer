e4e76de9b3343e1657023b96a19cb73a7d812264
Sean <seanmildenberger@gmail.com>
2022-12-04 22:39:12 -0700

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ import plotly.graph_objects as go
 app = dash.Dash(
 	external_stylesheets=[dbc.themes.BOOTSTRAP],
 	use_pages=True,
-	suppress_callback_exceptions=True
+	suppress_callback_exceptions=True,
+	pages_folder=''
 )
 
 server = app.server
